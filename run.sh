52910cfe980f29f332ca445d349b05f1750e4d4e
daniel <damor005@hotmail.com>
2025-11-22 10:44:37 +0100

    First commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+t=ultralytics/ultralytics:latest-jetson-jetpack6
+sudo docker run -it --ipc=host \
+	--runtime=nvidia \
+	--gpus all \
+	--device=/dev/video0:/dev/video0 \
+	-e DISPLAY=$DISPLAY \
+	-v /home/daniel/GolfTrolley_yolo:/user \
+	-v /temp/.X11-unix:/tmp/.X11-unix \
+	$t
